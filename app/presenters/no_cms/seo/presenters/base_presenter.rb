798021191e625db6b85ea4b6772250c05e67ef02
David J. Brenes <david.brenes@simplelogica.net>
2014-04-16 13:26:37 +0200

    BasePresenter now uses those default values

--- app/presenters/no_cms/seo/presenters/base_presenter.rb
+++ app/presenters/no_cms/seo/presenters/base_presenter.rb
@@ -1,11 +1,11 @@
 class NoCms::Seo::Presenters::BasePresenter
 
   def title
-    ""
+    NoCms::Seo.default_title
   end
 
   def description
-    ""
+    NoCms::Seo.default_description
   end
 
   def canonical_url
