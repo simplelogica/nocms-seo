5fe96a246c0458994ebc033a26da64252382372a
Jimena <jimena.escobar@the-cocktail.com>
2017-11-30 16:59:05 +0100

    Cambia etiquetas hechas como String por tag Cambia las etiquetas que hay que escapar formadas como un String por etiquetas formadas con tag()

--- app/helpers/no_cms/seo/seo_helper.rb
+++ app/helpers/no_cms/seo/seo_helper.rb
@@ -2,12 +2,12 @@ module NoCms::Seo::SeoHelper
 
   def canonical_url_tag canonical_url = ''
     canonical_url = canonical_url.blank? ? @seo_info.canonical_url(request.host_with_port) : canonical_url
-    "<link  rel = 'canonical' href = '#{canonical_url}' >" unless canonical_url.blank?
+    tag("link", rel: 'canonical', href: canonical_url) unless canonical_url.blank?
   end
 
   def meta_description_tag default_description = ''
     description = default_description.blank? ? @seo_info.description : default_description
-    "<meta name = 'description' content = '#{description}' >" unless description.blank?
+    tag("meta", name: 'description', content: description) unless description.blank?
   end
 
   def title_tag default_title = ''
@@ -23,7 +23,7 @@ module NoCms::Seo::SeoHelper
     content += ', '
     content += no_follow ? "nofollow" : "follow"
 
-    "<meta name = 'robots' content = '#{content}' >"
+    tag("meta", name: 'robots', content: content)
   end
 
 end
