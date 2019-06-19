c9ccedc59c740d11b729a9db0c26f6f89e7733e7
Luismi <luismiguel.vazquez@the-cocktail.com>
2019-06-19 10:29:23 +0200

    Fix migration to adapt to rails 4 and 5 sintax

--- spec/dummy/db/migrate/20140415144450_create_pages.rb
+++ spec/dummy/db/migrate/20140415144450_create_pages.rb
@@ -1,4 +1,11 @@
-class CreatePages < ActiveRecord::Migration
+active_record_migration_class =
+  if Rails::VERSION::STRING[0..2].to_f >= 5
+    ActiveRecord::Migration[Rails::VERSION::STRING[0..2].to_f]
+  else
+    ActiveRecord::Migration
+  end
+
+class CreatePages < active_record_migration_class
   def change
     create_table :pages do |t|
       t.string :title
