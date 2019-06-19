b2f2c32e5bb21ab05ec5b3f2c9e152ec01183798
Luismi <luismiguel.vazquez@the-cocktail.com>
2019-06-19 10:25:25 +0200

    ActiveRecord migrations now need to be tagged with the Rails version for Rails 5.2

--- db/migrate/20140415142547_create_no_cms_seo_infos.rb
+++ db/migrate/20140415142547_create_no_cms_seo_infos.rb
@@ -1,4 +1,11 @@
-class CreateNoCmsSeoInfos < ActiveRecord::Migration
+active_record_migration_class =
+  if Rails::VERSION::STRING[0..2].to_f >= 5
+    ActiveRecord::Migration[Rails::VERSION::STRING[0..2].to_f]
+  else
+    ActiveRecord::Migration
+  end
+
+class CreateNoCmsSeoInfos < active_record_migration_class
   def change
     create_table :no_cms_seo_infos do |t|
       t.belongs_to :target, polymorphic: true, index: true
--- db/migrate/20160218140633_add_kind_to_no_cms_seo_info.rb
+++ db/migrate/20160218140633_add_kind_to_no_cms_seo_info.rb
@@ -1,4 +1,11 @@
-class AddKindToNoCmsSeoInfo < ActiveRecord::Migration
+active_record_migration_class =
+  if Rails::VERSION::STRING[0..2].to_f >= 5
+    ActiveRecord::Migration[Rails::VERSION::STRING[0..2].to_f]
+  else
+    ActiveRecord::Migration
+  end
+
+class AddKindToNoCmsSeoInfo < active_record_migration_class
   def change
     add_column :no_cms_seo_infos, :kind, :string
     add_index :no_cms_seo_infos, [:target_id, :target_type, :kind], name: 'index_no_cms_seo_infos_kind', unique: true
